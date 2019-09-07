1444bdb502f5b81e6d83e675a300aa9500d49d80
Anna <anna.zueva.mail@inbox.ru>
2019-09-07 07:25:38 +0300

    8. Осн. Автоматизация - графика
    Изменила нек. псевдоэлементы на svg из спрайта.
    Исправила маркер на карте

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task("css", function () {
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
-    // .pipe(server.stream());
+    .pipe(server.stream());
 });
 
 gulp.task("images", function () {
